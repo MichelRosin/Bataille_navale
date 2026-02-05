d6e0870c0101b246d7c853f7278b9cfe3c690cef
MichelRosin <tguillot@enseignant-gscls.com>
2026-02-05 15:28:33 +0100

    Initialisation - chargement des fichiers
    Importation des premiers fichiers du jeu

---
+++ sw-battleship.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'battleship-v1';
+const urlsToCache = [
+  './battleship.html',
+  './battleship-game.js',
+  './manifest-battleship.json',
+  './questions.json'
+];
+
+// Installation
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Erreur de mise en cache:', err);
+      })
+  );
+});
+
+// Activation
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
